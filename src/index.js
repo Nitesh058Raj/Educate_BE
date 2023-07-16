43ff0ccd68253581dd1e5f0b2e17ff1bcf1b3da0
WinayGupta777 <vinay.gupta8101@gmail.com>
2023-07-16 22:48:14 +0530

    Added cors library;

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import chalk from "chalk";
+import cors from "cors";
 
 import { debugLog } from "./utils/logging.util.js";
 import routes from "./routes/routes.js";
@@ -7,11 +8,12 @@ import routes from "./routes/routes.js";
 const PORT = process.env.PORT || 5000;
 const app = express();
 
+app.use(cors());
+app.use("/api", routes);
+
 app.get("/", (req, res) => {
   res.send("API WORKING FINE!");
 });
-
-app.use("/api", routes);
 
 app.listen(PORT, () => {
   debugLog(`Server is running on port ${chalk.green(PORT)}`);
