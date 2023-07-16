995894e9f7b81116a1ed1b9cb70e721bd09d71b1
WinayGupta777 <vinay.gupta8101@gmail.com>
2023-07-16 18:00:25 +0530

    Added dotenv;

--- src/configs/database.config.js
+++ src/configs/database.config.js
@@ -1,14 +1,14 @@
 import mysql from "mysql2";
+import dotenv from "dotenv";
 
-const pool = await mysql
-  .createPool({
-    host: process.env.MYSQL_HOST || "localhost",
-    user: process.env.MYSQL_USER || "root",
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
-    waitForConnections: true,
-    connectionLimit: 10,
-  })
-  .promise();
+dotenv.config();
+const pool = mysql.createPool({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USERNAME,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  waitForConnections: true,
+  connectionLimit: 10,
+});
 
 export default pool;
